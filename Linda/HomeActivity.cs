5bd3d555faeb3c02eacfb54f95cf09c59f85c0f9
Jay Rainey <jawrainey@gmail.com>
2016-08-02 13:26:58 +0100

    Added swipe-able home activity

---
+++ Linda/HomeActivity.cs
@@ -0,0 +1,67 @@
+using Android.App;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using System;
+using System.Collections.Generic;
+using Android.Views;
+
+namespace Linda
+{
+	[Activity]
+	public class HomeActivity : FragmentActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.home);
+			// ??
+			var adaptor = new FragmentAdapter(SupportFragmentManager);
+			// ??
+			adaptor.AddFragmentView((i, v, _) => { return i.Inflate(Resource.Layout.frag_home, v, false); });
+			adaptor.AddFragmentView((i, v, _) => { return i.Inflate(Resource.Layout.frag_collect, v, false); });
+			adaptor.AddFragmentView((i, v, _) => { return i.Inflate(Resource.Layout.frag_topic, v, false); });
+			adaptor.AddFragmentView((i, v, _) => { return i.Inflate(Resource.Layout.frag_record, v, false); });
+			// ??
+			FindViewById<ViewPager>(Resource.Id.pager).Adapter = adaptor;
+		}
+	}
+
+	public class FragmentAdapter : FragmentPagerAdapter
+	{
+		readonly List<Android.Support.V4.App.Fragment> _fragments = new List<Android.Support.V4.App.Fragment>();
+
+		public FragmentAdapter(Android.Support.V4.App.FragmentManager fm) : base(fm) { }
+
+		public override int Count 
+		{
+			get { return _fragments.Count; }
+		}
+
+		public override Android.Support.V4.App.Fragment GetItem(int position)
+		{
+			return _fragments[position];
+		}
+
+		public void AddFragmentView(Func<LayoutInflater, ViewGroup, Bundle, View> view)
+		{
+			_fragments.Add(new GenericViewPagerFragment(view));
+		}
+	}
+
+	public class GenericViewPagerFragment : Android.Support.V4.App.Fragment
+	{
+		readonly Func<LayoutInflater, ViewGroup, Bundle, View> _view;
+
+		public GenericViewPagerFragment(Func<LayoutInflater, ViewGroup, Bundle, View> view)
+		{
+			_view = view;
+		}
+
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			base.OnCreateView(inflater, container, savedInstanceState);
+			return _view(inflater, container, savedInstanceState);
+		}
+	}
+}
