ae5df939de6b02faf25e8e32f1120df4711f3cdc
Jay Rainey <jawrainey@gmail.com>
2017-02-19 23:34:12 +0000

    Add PagerSnap support for recyclerview
    - Removed unnecessary callback
    - Added click event to prompt, which is implemented inside the activity.
    - Resized prompt layout upon inflating view

--- Gabber/Activities/PromptSelectionActivity.cs
+++ Gabber/Activities/PromptSelectionActivity.cs
@@ -3,63 +3,47 @@ using Android.Content;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Support.V7.Widget;
-using Android.Support.V7.Widget.Helper;
 using Android.Widget;
-using Android.Support.Design.Widget;
 using FFImageLoading.Views;
 using GabberPCL;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
 
 namespace Gabber
 {
 	[Activity(Label = "Topics to discuss")]
 	public class PromptSelectionActivity : AppCompatActivity
 	{
+		RecyclerView promptRecyclerView;
+
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.promptselection);
-			SetSupportActionBar(FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar));
+			SetSupportActionBar(FindViewById<Toolbar>(Resource.Id.toolbar));
 
 			var model = new DatabaseManager(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal));
-			// As data is stored entirely in JSON, we retrieve all projects to filter manually.
-			// Filter the list based on the previously selected theme
 			var selectedProject = model.GetProjects().Find((Project pj) => pj.theme == Intent.GetStringExtra("theme"));
-			// Set appropriate prompts for the selected project
-			var recyclerView = FindViewById<RecyclerView>(Resource.Id.prompts);
-			recyclerView.SetAdapter(new RVPromptAdapter(selectedProject.prompts));
 
-			// Custom layout required to disable vertical scrolling.
-			recyclerView.SetLayoutManager(new CustomLinearLayoutManager(this));
-			// Handles the "swipe to dismiss" ability that is incorporated as prompt-cards.
-			var callback = new PromptSelectorCallback(0, ItemTouchHelper.Left | ItemTouchHelper.Right);
-			var touchHelper = new ItemTouchHelper(callback);
-			touchHelper.AttachToRecyclerView(recyclerView);
+			promptRecyclerView = FindViewById<RecyclerView>(Resource.Id.prompts);
+			promptRecyclerView.SetLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.Horizontal, false));
 
-			// Make it obvious how to select a discussion prompt.
-			Snackbar.Make(recyclerView, "Select a topic to guide the interview", Snackbar.LengthLong).Show();
-
-			FindViewById<ImageButton>(Resource.Id.selectFAB).Click += delegate
-			{
-				// Given the selected item is the first item in the view
-				var selectedPrompt = recyclerView.FindViewById(Resource.Id.promptCard);
-				var promptImage = selectedPrompt.FindViewById<ImageViewAsync>(Resource.Id.imagePrompt);
-				var promptText = selectedPrompt.FindViewById<TextView>(Resource.Id.caption).Text;
-				// All the previous form data and selected prompt.
-				var intent = new Intent(this, typeof(RecordStoryActivity));
-				// The tag is the drawable resource ID, which is a Java object, hence conversion and cast.
-				intent.PutExtra("promptImage", promptImage.Tag.ToString());
-				intent.PutExtra("promptText", promptText);
-				// Pass the previous form data (photo/name/email)
-				intent.PutExtras(Intent.Extras);
-				StartActivity(intent);
-			};
+			var adapter = new RVPromptAdapter(selectedProject.prompts);
+			adapter.ProjectClicked += ProjectSelected;
+			promptRecyclerView.SetAdapter(adapter);
+			// This allows us to use recyclerview similar to a ViewPager
+			new PagerSnapHelper().AttachToRecyclerView(promptRecyclerView);
 		}
-	}
 
-	public class CustomLinearLayoutManager : LinearLayoutManager
-	{
-		public CustomLinearLayoutManager(Context c) : base(c) { }
-		// Disable scrolling within the layout container that shows a swipable item.
-		public override bool CanScrollVertically() { return false; }
+		void ProjectSelected(object sender, int position)
+		{
+			var prompt = promptRecyclerView.GetLayoutManager().FindViewByPosition(position).FindViewById(Resource.Id.promptCard);
+			// All the previous form data and selected prompt.
+			var intent = new Intent(this, typeof(RecordStoryActivity));
+			intent.PutExtra("promptImage", prompt.FindViewById<ImageViewAsync>(Resource.Id.imagePrompt).Tag.ToString());
+			intent.PutExtra("promptText", prompt.FindViewById<TextView>(Resource.Id.caption).Text);
+			// Pass the previous form data
+			intent.PutExtras(Intent.Extras);
+			StartActivity(intent);
+		}
 	}
 }
--- Gabber/Adapters/RVPromptAdapter.cs
+++ Gabber/Adapters/RVPromptAdapter.cs
@@ -4,6 +4,7 @@ using Android.Widget;
 using System.Collections.Generic;
 using FFImageLoading;
 using FFImageLoading.Views;
+using System;
 
 namespace Gabber
 {
@@ -20,7 +21,12 @@ namespace Gabber
 		public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
 		{
 			View row = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.prompt, parent, false);
-			return new PhotoViewHolder(row);
+			// Modify prompt size (60% of screen); required as the view is inflated inside another layout, 
+			// so it is not possible (?) to modify the size of the prompt layout otherwise.
+			var parms = row.LayoutParameters;
+			parms.Width = (int)(parent.Width * 0.6);
+			row.LayoutParameters = parms;
+			return new PhotoViewHolder(row, OnProjectClick);
 		}
 		
 		public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
@@ -38,13 +44,21 @@ namespace Gabber
 			get { return _prompts.Count; }
 		}
 
+		public event EventHandler<int> ProjectClicked;
+
+		void OnProjectClick(int position)
+		{
+			if (ProjectClicked != null) ProjectClicked(this, position);
+		}
+
 		public class PhotoViewHolder : RecyclerView.ViewHolder
 		{
 			public ImageViewAsync Image { get; set; }
 			public TextView Caption { get; set; }
 
-			public PhotoViewHolder(View itemView) : base(itemView)
+			public PhotoViewHolder(View itemView, Action<int> listener) : base(itemView)
 			{
+				itemView.Click += (sender, e) => listener(LayoutPosition);
 				Image = itemView.FindViewById<ImageViewAsync>(Resource.Id.imagePrompt);
 				Caption = itemView.FindViewById<TextView>(Resource.Id.caption);
 			}
