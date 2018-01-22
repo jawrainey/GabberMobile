30151ab61371993c46590fb49d4b3082a6944f82
Jay Rainey <jawrainey@gmail.com>
2018-01-22 11:31:49 +0000

    Committing initial iOS project

---
+++ Gabber.iOS/Controllers/LoginViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Gabber.iOS
+{
+    public partial class LoginViewController : UIViewController
+    {
+        public LoginViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ Gabber.iOS/ProjectViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Gabber.iOS
+{
+    public partial class ProjectViewController : UICollectionViewController
+    {
+        public ProjectViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
