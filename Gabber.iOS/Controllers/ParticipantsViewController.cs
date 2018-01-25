d6d76680c8c74468067d190ad73a310f75a2b611
Jay Rainey <jawrainey@gmail.com>
2018-01-25 14:45:32 +0000

    Refactored how new participants are added
    Abstracted database access to a static, PCL class such that these can
    be tested on the PCL and shared between iOS and Android in the future.

--- Gabber.iOS/Controllers/ParticipantsViewController.cs
+++ Gabber.iOS/Controllers/ParticipantsViewController.cs
@@ -10,15 +10,17 @@ namespace Gabber.iOS
 {
     public partial class ParticipantsViewController : UIViewController
     {
+        ParticipantsCollectionViewSource participantsViewSource;
+
         public ParticipantsViewController (IntPtr handle) : base (handle) {}
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
-            var participants = GetParticipants();
+            var _participants = Queries.AllParticipants();
 
-            if (participants.Count <= 0)
+            if (_participants.Count <= 0)
             {
                 Session.Connection.Insert(new Participant
                 {
@@ -27,35 +29,20 @@ namespace Gabber.iOS
                     Selected = true
                 });
             }
-            PopulateCollectionView();
+
+            participantsViewSource = new ParticipantsCollectionViewSource(_participants);
+            ParticipantsCollectionView.Source = participantsViewSource;
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            participantsViewSource.Rows = Queries.AllParticipants();
+            ParticipantsCollectionView.ReloadData();
         }
 
         // Revisited this page, i.e. after adding a participant
         [Action("UnwindToParticipantsViewController:")]
-        public void UnwindToParticipantsViewController(UIStoryboardSegue segue) => PopulateCollectionView();
-
-        // Simplified accessor 
-        List<Participant> GetParticipants() => Session.Connection.Table<Participant>().ToList();
-
-        // Due to the required hack below, the ViewSource must be updated. 
-        // TODO: using an observer collection could remove the need to reassign participants.
-        void PopulateCollectionView()
-        {
-            var participants = GetParticipants();
-            // TODO: this is used here as we need to update the ViewSource once the page is re-visited (i.e. ViewWillAppear)
-            ParticipantsCollectionView.Source = new ParticipantsCollectionViewSource(participants);
-            // The selection state is mirror the participants on form load, then we can
-            // update the UI state in ViewSource and not worry about the row model.
-            // HACK: this was required as all rows are false by default, which meant if a participant was
-            // selected (such as from a previous use of the app), then the UI would update (via ViewCell),
-            // but a double-click in ItemSelected is required to show the DeSelected state.
-            for (int i = 0; i < participants.Count; i++)
-            {
-                if (participants[i].Selected)
-                {
-                    ParticipantsCollectionView.SelectItem(NSIndexPath.FromRowSection(i, 0), false, 0);
-                }
-            }
-        }
+        public void UnwindToParticipantsViewController(UIStoryboardSegue segue) {}
     }
 }
---
+++ GabberPCL/Queries.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SQLite;
+
+namespace GabberPCL
+{
+    public static class Queries
+    {
+        public static List<Participant> AllParticipants() => Session.Connection.Table<Participant>().ToList();
+        public static List<Participant> SelectedParticipants() => AllParticipants().FindAll((p) => p.Selected);
+    }
+}
