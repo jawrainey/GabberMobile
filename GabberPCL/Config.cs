554d2c316af9b32db7d9be6bae8df6abf2b0c8fe
Jay Rainey <jawrainey@gmail.com>
2018-09-13 09:09:23 +0200

    Accidentally committed ngrok configuration

--- GabberPCL/Config.cs
+++ GabberPCL/Config.cs
@@ -2,14 +2,14 @@
 {
     public static class Config
     {
-        static readonly string BASE_URL = "ca704a99.ngrok.io";
+        static readonly string BASE_URL = "dev.gabber.audio";
 
         public static string WEB_URL = "https://" + BASE_URL;
         public static string PRINT_URL = "www." + BASE_URL;
         public static string ABOUT_URL = WEB_URL + "/about/";
         public static string ABOUT_DATA_PAGE = WEB_URL + "/research";
 
-        public static string API_ENDPOINT = "https://" + BASE_URL;
+        public static string API_ENDPOINT = "https://api" + BASE_URL;
 
         public static string DATABASE_NAME = "gabber.db3";
     }
